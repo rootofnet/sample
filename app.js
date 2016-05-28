1bc22c3b05654fff0e911cebf2f668ddbc5d1d41
rootofnet <hocha9938@daum.net>
2016-05-28 16:32:49 +0900

    commit

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 
+/********************************************************************************/
 
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/',function(req,res) {
-  res.send('Hello World!');
+/********************************************************************************/
+
+app.set('view engine', 'ejs'); // express는 디폴트 views 폴더안의 ejs를 등록한다.
+
+/********************************************************************************/
+
+// app.use 는 filter와 같은 역할을 한다.
+
+// index 지정
+// app.get('/',function(req,res) {
+//   res.send('Hello World!');
+// });
+app.use(express.static(path.join(__dirname, 'public')));
+
+/********************************************************************************/
+var v_data = {i_count:0};
+
+// http://localhost:3000/p_count=50
+app.get('/sample',function(req,res) {
+  if(req.query.p_count) {
+    v_data.i_count = req.query.p_count;
+  } else {
+    v_data.i_count++;
+  }
+  res.render('first_ejs', v_data);
 });
+
+// http://localhost:3000/sample/1000
+app.get('/sample/:xxx',function(req,res) {
+  v_data.i_count = req.params.xxx;
+  res.render('first_ejs', v_data);
+});
+
+/********************************************************************************/
 
 app.listen(3000,function() {
   console.log('Server On!');
-  console.log('Server On!');
-  console.log('Server On!');  
 });
+
+/********************************************************************************/
